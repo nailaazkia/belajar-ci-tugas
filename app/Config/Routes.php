c12596f9d3df2118440ca748e47992a7bd9aff4f
nailaazkia <nailaazkia57@gmail.com>
2025-06-25 15:16:52 +0700

    menambahkan api data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -21,6 +21,13 @@ $routes->group('produk', ['filter' => 'auth'], function ($routes) {
     $routes->get('download', 'ProdukController::download');
 });
 
+$routes->group('product-category', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'ProdukCategoryController::index');
+    $routes->post('', 'ProdukCategoryController::store');  // ubah dari 'store' ke ''
+    $routes->post('edit/(:num)', 'ProdukCategoryController::edit/$1');
+    $routes->get('delete/(:num)', 'ProdukCategoryController::delete/$1');
+});
+
 $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->get('', 'TransaksiController::index');
     $routes->post('', 'TransaksiController::cart_add');
@@ -35,17 +42,12 @@ $routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
 $routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
 $routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
 
-$routes->group('product-category', ['filter' => 'auth'], function ($routes) {
-    $routes->get('', 'ProdukCategoryController::index');
-    $routes->post('', 'ProdukCategoryController::store');  // ubah dari 'store' ke ''
-    $routes->post('edit/(:num)', 'ProdukCategoryController::edit/$1');
-    $routes->get('delete/(:num)', 'ProdukCategoryController::delete/$1');
-});
-
 $routes->get('product-category', 'ProdukCategoryController::index', ['as' => 'product-category']);
 
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 
+$routes->get('profile', 'Home::profile', ['filter' => 'auth']);
 $routes->get('contact', 'FaqController::index', ['filter' => 'auth']);
 $routes->get('faq', 'FaqController::index', ['filter' => 'auth']);
-$routes->get('profil', 'FaqController::index', ['filter' => 'auth']);
+
+$routes->resource('api', ['controller' => 'apiController']);
